e013ffa801e45eeceec00daf81187ce67e9c2d31
Ambuj <thecoderguy@outlook.com>
2018-07-19 02:54:00 +0530

    search by order of time

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const request = require('request');
 app.set("view engine", "ejs");
 app.get("/", function(req, res){
-    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&order=date&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var parse = JSON.parse(body);
