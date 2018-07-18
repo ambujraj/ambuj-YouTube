c4c27537f7bfa40391b2b859ba7d587db60abb03
Ambuj <thecoderguy@outlook.com>
2018-07-19 02:28:15 +0530

    final-upload

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 const request = require('request');
 app.set("view engine", "ejs");
 app.get("/", function(req, res){
-    res.render("landing");
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNj8krJYML-KG22xyumvMww&type=video&key=AIzaSyA58s6f45ex23jhsW0m54CpjhD71a4bJgc';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var parse = JSON.parse(body);
+            res.render("landing", {body: parse});
+        }
+    });
+    
 });
 app.get("*", function(req, res){
     res.redirect("/");
